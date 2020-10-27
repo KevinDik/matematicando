e4056fa2105aa903c1b6978d0a669bf756ad2022
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2020-10-27 09:55:49 -0300

    definicao das funcoes operacao
    criacao da logica de todas as operacoes

--- modelos/calcular.py
+++ modelos/calcular.py
@@ -44,14 +44,44 @@ class Calcular:
 
     @property
     def _gerar_valor(self):
-        pass
+        if self.dificuldade == 1:
+            return randint(0, 10)
+        elif self.dificuldade == 2:
+            return randint(0, 100)
+        elif self.dificuldade == 3:
+            return randint(0, 1000)
+        elif self.dificuldade == 4:
+            return randint(0, 10000)
+        else:
+            return randint(0, 100000)
 
     @property
     def _gerar_resultado(self):
-        pass
+        if self.operacao == 1:
+            return self.valor1 + self.valor2
+        elif self.operacao == 2:
+            return self.valor1 - self.valor2
+        else:
+            self.valor1 * self.valor2
 
     def checar_resultado(self, resposta):
-        pass
+        certo = False
+        if self.resultado == resposta:
+            print('Resposta correta')
+            certo = True
+        else:
+            print('Resposta errada')
+        print(f'{self.valor1} {self._op_simbolo} {self.valor2} = {self.resultado}')
+        return certo
+
+    @property
+    def _op_simbolo(self):
+        if self.operacao == 1:
+            return '+'
+        elif self.operacao == 2:
+            return '-'
+        else:
+            return '*'
 
     def mostrar_operacao(self):
-        pass
+        print(f'{self.valor1} {self._op_simbolo} {self.valor2} = ?')
